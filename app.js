66a5a403245ab1c321ebce3edca59496a57f8ac4
Ming Luo <luoming886@gmail.com>
2020-12-31 12:42:56 +1300

    Split route logics into admin and shop routes

--- app.js
+++ app.js
@@ -1,18 +1,15 @@
-const http = require('http')
-
+const http = require('http');
 const express = require('express');
-
 const app = express();
 
-app.use('/add-product', (req, res, next) => {
-    console.log('In the middleware!');
-    res.send('<h1>Add product page</h1>');
-})
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-app.use('/', (req, res, next) => {
-    console.log('In the middleware!');
-    res.send('<h1>Main page<h1>');
-})
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(adminRoutes);
+app.use(shopRoutes);
 
 const server = http.createServer(app);
 
