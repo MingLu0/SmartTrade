313609bc5aabdf39b07ca25403bd23c397f5b97e
Ming Luo <luoming886@gmail.com>
2021-01-03 17:31:39 +1300

    Created 404 html

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
 const http = require('http');
 const express = require('express');
 const app = express();
+const path = require('path')
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 const bodyParser = require('body-parser');
 
-app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next)=>{
-    res.status(400).send('<h1>Page Not Found</h1>')
+app.use((req, res, next) => {
+    res.status(400).sendFile(path.join(__dirname, 'views', '404.html'))
 })
 
 const server = http.createServer(app);
