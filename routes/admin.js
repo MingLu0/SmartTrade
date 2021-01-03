16c7044470e1a9c2201f8ec5756d4d059894b9d0
Ming Luo <luoming886@gmail.com>
2021-01-03 17:03:17 +1300

    Used path to direct to different html pages in the middleware

--- routes/admin.js
+++ routes/admin.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const path = require('path')
 
 router.get('/add-product', (req, res, next) => {
-    console.log('In the middleware!');
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit" title="add" />');
+    res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
 })
 
 router.post('/product', (req, res, next) => {
