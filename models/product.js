5992b17e1c7523ec04f0917dfa628dd7ab360194
Ming Luo <luoming886@gmail.com>
2021-01-09 15:46:01 +1300

    Refactored code to extract the get product from file logic

--- models/product.js
+++ models/product.js
@@ -3,19 +3,28 @@ const rootDir = require("../util/path");
 const path = require("path");
 const fs = require("fs");
 
+const p = path.join(
+  path.dirname(process.mainModule.filename),
+  "data",
+  "products.json"
+);
+
+const getProductsFromFile = (callBack) => {
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      callBack([]);
+    } else {
+      callBack(JSON.parse(fileContent));
+    }
+  });
+};
+
 module.exports = class Product {
   constructor(t) {
     this.title = t;
   }
-
   save() {
-    const p = path.join(path.dirname(process.mainModule.filename), "data", "products.json");
-
-    fs.readFile(p, (err, fileContent) => {
-      let products = [];
-      if (!err) {
-        products = JSON.parse(fileContent);
-      }
+    getProductsFromFile((products) => {
       products.push(this);
       fs.writeFile(p, JSON.stringify(products), (err) => {
         console.log(err);
@@ -24,13 +33,6 @@ module.exports = class Product {
   }
 
   static fetchAll(callBack) {
-    const p = path.join(path.dirname(process.mainModule.filename), "data", "products.json");
-
-    fs.readFile(p, (err, fileContent) => {
-      if (err) {
-        callBack([]);
-      }
-       callBack(JSON.parse(fileContent));
-    });
+    getProductsFromFile(callBack);
   }
 };
